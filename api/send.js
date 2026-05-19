a95f4d446a7be6f1ad0a91f7625f8007fb6ab8dc
WazKuz <155897901+WazKuz@users.noreply.github.com>
2026-05-19 13:05:10 +0100

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,1 +1,30 @@
+import fs from "fs";
 
+const FILE = "./data/messages.json";
+
+function read() {
+  if (!fs.existsSync(FILE)) return {};
+  return JSON.parse(fs.readFileSync(FILE));
+}
+
+function write(data) {
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+}
+
+export default function handler(req, res) {
+  const { roomKey, username, message } = req.body;
+
+  const messages = read();
+
+  if (!messages[roomKey]) messages[roomKey] = [];
+
+  messages[roomKey].push({
+    username,
+    message,
+    time: Date.now()
+  });
+
+  write(messages);
+
+  res.json({ success: true });
+}
